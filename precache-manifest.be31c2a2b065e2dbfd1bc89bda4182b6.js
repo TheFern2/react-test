4a40e83502be97f7ba52703422866540bbe318fa
kodaman2 <fredz0003@gmail.com>
2019-09-14 02:38:38 -0500

    Updates

---
+++ precache-manifest.be31c2a2b065e2dbfd1bc89bda4182b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54fbe759006e4585275ee184e35f7907",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "ac383ff3991addb8afcb",
+    "url": "/react-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "161ad27b6f805e6d84da",
+    "url": "/react-test/static/js/2.14efcdec.chunk.js"
+  },
+  {
+    "revision": "ac383ff3991addb8afcb",
+    "url": "/react-test/static/js/main.34a2265d.chunk.js"
+  },
+  {
+    "revision": "921b74090cdafbb44d29",
+    "url": "/react-test/static/js/runtime~main.91b23537.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
